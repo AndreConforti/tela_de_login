aa8b4d77f705fc51496fd8c97ae08c6c75f3f0dc
AndreConforti <andre.r.m.conforti@gmail.com>
2022-03-13 22:29:20 -0300

    primeiros passos
    Formulários criados e primeiros comandos prontos.
    Próximo passo, conectar ao Banco de Dados.

--- principal.py
+++ principal.py
@@ -1,115 +1,65 @@
-from cmath import log
 from PyQt5 import QtWidgets, uic
-import mysql.connector
 
-banco = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    passwd = '',
-    database = 'banco_cadastro'
-)
 
 def verificar_login():
-    login.lbl_aviso.setText("")
-    usuario = login.lne_usuario.text().upper().strip()
-    senha = login.lne_senha.text().strip()
-
-    try:
-        cursor = banco.cursor()
-        cursor.execute("SELECT senha FROM cadastro WHERE login = '{}'".format(usuario))
-        senha_bd = cursor.fetchall()
-        print(senha_bd[0][0])
-    except:
-        login.lbl_aviso.setText("Usuário não está cadastrado no Sistema")
-
-    '''if senha == senha_bd[0][0]:
-        login.close()
+    principal.lbl_aviso.setText('')
+    usuario = principal.lne_usuario.text()
+    senha = principal.lne_senha.text()
+    if usuario == 'andre' and senha == '123':
+        principal.lne_usuario.setText('')
+        principal.lne_senha.setText('')
+        principal.close()
         logado.show()
     else:
-        login.lbl_aviso.setText("Senha não confere")'''
-
-def sair_do_sistema():
-    login.close()
+        principal.lbl_aviso.setText('Usuário ou Senha incorretos')
 
 
 def logout():
     logado.close()
-    login.show()
+    principal.show()
 
 
-def chamar_cadastro():
-    login.lbl_aviso.setText("")
-    login.lne_usuario.setText("")
-    login.lne_senha.setText("")
-    login.close()
-    cadastro.show()
+def sair_do_sistema():
+    principal.close()
 
 
 def cadastrar_usuario():
-    nome = cadastro.lne_nome.text()
-    usuario = cadastro.lne_usuario.text()
-    senha = cadastro.lne_senha.text()
-    c_senha = cadastro.lne_rptsenha.text()
-
-    if senha == c_senha:
-        try:
-            cursor = banco.cursor()
-            cursor.execute("INSERT INTO cadastro VALUES ('"+nome+"', '"+usuario+"', '"+senha+"')")
-
-            banco.commit()
-            banco.close()
-            cadastro.lbl_aviso.setText("Usuário cadastrado com sucesso!")
-        
-        except mysql.connector.Error as erro:
-            print("Erro ao inserir os dados: ", erro)
-
-    else:
-        cadastro.lbl_aviso.setText("As senhas estão diferentes. Repita a senha corretamente")
-
-
-def verificar_cadastro():
-    cadastro.close()
-    confirma_cadastro.show()
+    principal.lbl_aviso.setText('')
+    principal.lne_usuario.setText('')
+    principal.lne_senha.setText('')
+    principal.close()
+    cadastro.show()
 
 
 def cancelar_cadastro():
-    cadastro.lne_nome.setText("")
-    cadastro.lne_usuario.setText("")
-    cadastro.lne_senha.setText("")
-    cadastro.lne_rptsenha.setText("")
-    cadastro.lbl_aviso.setText("")
     cadastro.close()
-    login.show()
+    principal.show()
 
 
-def limpar_cadastro():
-    cadastro.lne_nome.setText("")
-    cadastro.lne_usuario.setText("")
-    cadastro.lne_senha.setText("")
-    cadastro.lne_rptsenha.setText("")
-    cadastro.lbl_aviso.setText("")
-
-def voltar_login():
-    confirma_cadastro.close()
-    login.show()
+def limpar_campos():
+    cadastro.lne_nome.setText('')
+    cadastro.lne_usuario.setText('')
+    cadastro.lne_senha.setText('')
+    cadastro.lne_rptSenha.setText('')
 
 
 app = QtWidgets.QApplication([])
 
-login = uic.loadUi('login.ui')
+# Associar os formulários a uma variável, para poder chamá-los no sistema
+principal = uic.loadUi('login.ui')
 logado = uic.loadUi('logado.ui')
 cadastro = uic.loadUi('cadastro_usuario.ui')
-confirma_cadastro = uic.loadUi('verificar_cadastro.ui')
+cadastro_realizado = uic.loadUi('verificar_cadastro.ui')
 
-login.btn_entrar.clicked.connect(verificar_login)
-login.btn_sair.clicked.connect(sair_do_sistema)
-login.btn_cadastrar.clicked.connect(chamar_cadastro)
+# Associar os botões dos formulários a uma função específica
+principal.btn_entrar.clicked.connect(verificar_login)
+principal.btn_sair.clicked.connect(sair_do_sistema)
+principal.btn_cadastrar.clicked.connect(cadastrar_usuario)
 logado.btn_encerrar.clicked.connect(logout)
-cadastro.btn_cadastrar.clicked.connect(cadastrar_usuario)
 cadastro.btn_cancelar.clicked.connect(cancelar_cadastro)
-cadastro.btn_limpar.clicked.connect(limpar_cadastro)
-confirma_cadastro.btn_voltar.clicked.connect(voltar_login)
+cadastro.btn_limpar.clicked.connect(limpar_campos)
 
 
-login.show()
+# Início do programa 
+principal.show()
 app.exec()
